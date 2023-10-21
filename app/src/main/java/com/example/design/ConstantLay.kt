b2951781c57b14827d16fb0e4513cdb32a2bcd5d
rumman <cset-210652@dti.ac>
2023-10-21 11:21:04 +0600

    Design

---
+++ app/src/main/java/com/example/design/ConstantLay.kt
@@ -0,0 +1,11 @@
+package com.example.design
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstantLay : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constant_lay)
+    }
+}
